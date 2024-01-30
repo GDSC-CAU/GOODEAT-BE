427c4746bc22691abf72393704dd04d487d75bc7
hong-sile <hyukjoon.dev@gmail.com>
2024-01-30 14:51:00 +0900

    init project

---
+++ src/main/java/com/gdsc/goodeat/GoodeatApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.goodeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodeatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdsc/goodeat/GoodeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.goodeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
