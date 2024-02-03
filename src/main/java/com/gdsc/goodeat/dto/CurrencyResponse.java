0aa281b6e0e0813c4479ce3f64f21bd9167af71f
tthisag246 <tthisag246@gmail.com>
2024-02-03 22:52:54 +0900

    feat: currency response를 반환하는 기능 추가
    언어목록 반환 API 참고
    
    #6

---
+++ src/main/java/com/gdsc/goodeat/dto/CurrencyResponse.java
@@ -0,0 +1,24 @@
+package com.gdsc.goodeat.dto;
+
+import com.gdsc.goodeat.domain.Currency;
+
+public class CurrencyResponse {
+
+  private final String currencyName;
+
+  private CurrencyResponse() {
+    this(null);
+  }
+
+  public CurrencyResponse(final String currencyName) {
+    this.currencyName = currencyName;
+  }
+
+  public static CurrencyResponse from(final Currency currency){
+    return new CurrencyResponse(currency.getCurrencyName());
+  }
+
+  public String getCurrencyName() {
+    return currencyName;
+  }
+}
