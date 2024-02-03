6f2785b16f27ef4b619ee932e6671efe82b824d7
hong-sile <hyukjoon.dev@gmail.com>
2024-02-03 16:58:05 +0900

    feat: language 목록을 response 형태로 변환하는 기능 구현
    #2

---
+++ src/test/java/com/gdsc/goodeat/service/LanguageServiceTest.java
@@ -0,0 +1,29 @@
+package com.gdsc.goodeat.service;
+
+import static com.gdsc.goodeat.domain.Language.AFRIKAANS;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.gdsc.goodeat.domain.Language;
+import com.gdsc.goodeat.dto.LanguageResponse;
+import com.gdsc.goodeat.support.ServiceTest;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class LanguageServiceTest extends ServiceTest {
+
+  @Autowired
+  private LanguageService languageService;
+
+  @Test
+  void 언어_목록을_조회할_수_있다(){
+    //given, when
+    final LanguageResponse expectedElement = LanguageResponse.from(AFRIKAANS);
+    final List<LanguageResponse> actual = languageService.findAllLanguages();
+
+    //then
+    assertThat(actual)
+        .usingRecursiveFieldByFieldElementComparator()
+        .contains(expectedElement);
+  }
+}
