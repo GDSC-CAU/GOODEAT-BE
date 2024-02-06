055e61a6daad852b35aeab772a5a37e26c17d26d
hong-sile <hyukjoon.dev@gmail.com>
2024-02-06 18:37:27 +0900

    refactor: profile 별로 TranslationClient를 사용하도록 분리
    #9

--- src/main/java/com/gdsc/goodeat/external/GoogleTranslationClient.java
+++ src/main/java/com/gdsc/goodeat/external/GoogleTranslationClient.java
@@ -3,14 +3,16 @@ package com.gdsc.goodeat.external;
 import com.gdsc.goodeat.domain.Language;
 import com.gdsc.goodeat.domain.TranslationClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("!test")
 public class GoogleTranslationClient implements TranslationClient {
 
   public final String apiKey;
 
-  public GoogleTranslationClient(@Value("${apikey.google}") final String apiKey) {
+  public GoogleTranslationClient(@Value("${apikey.google}: not prod") final String apiKey) {
     this.apiKey = apiKey;
   }
 
