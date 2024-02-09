724b03a541ba6677e63c85a226677070fe5c98f4
hong-sile <hyukjoon.dev@gmail.com>
2024-02-09 11:48:54 +0900

    feat: 번역 기능 추가
    #9

--- src/main/java/com/gdsc/goodeat/external/GoogleTranslationClient.java
+++ src/main/java/com/gdsc/goodeat/external/GoogleTranslationClient.java
@@ -2,21 +2,50 @@ package com.gdsc.goodeat.external;
 
 import com.gdsc.goodeat.domain.Language;
 import com.gdsc.goodeat.domain.TranslationClient;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.Translate.TranslateOption;
 import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 @Component
 @Profile("!test")
 public class GoogleTranslationClient implements TranslationClient {
 
-  public final Translate translate;
+  private final Translate translate;
 
-  public GoogleTranslationClient() {
-    translate = TranslateOptions.getDefaultInstance().getService();
+  public GoogleTranslationClient(
+      @Value("${google.credential.path:not prod}") final String path,
+      @Value("${spring.profile.active}") final String profile
+  ) {
+    if (profile.equals("prod")) {
+      translate = initTranslateIfProd(path);
+    } else {
+      translate = new EmptyTranslate();
+    }
+  }
+
+  private Translate initTranslateIfProd(final String path) {
+    final Translate translate;
+    try (final InputStream serviceStream = new ClassPathResource(path).getInputStream()) {
+      final ServiceAccountCredentials credential = ServiceAccountCredentials
+          .fromStream(serviceStream);
+
+      translate = TranslateOptions.newBuilder()
+          .setCredentials(credential)
+          .build()
+          .getService();
+
+    } catch (final IOException e) {
+      throw new RuntimeException(e);
+    }
+    return translate;
   }
 
   public String translate(final Language source, final Language target, final String content) {
@@ -24,6 +53,7 @@ public class GoogleTranslationClient implements TranslationClient {
         content
         , TranslateOption.sourceLanguage(source.getISO639Code())
         , TranslateOption.targetLanguage(target.getISO639Code())
+        , TranslateOption.model("base")
     );
     return translation.getTranslatedText();
   }
