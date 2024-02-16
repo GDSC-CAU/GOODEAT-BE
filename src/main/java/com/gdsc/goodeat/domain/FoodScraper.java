4d1aae52af07d58e44449e94d725933ca48e790a
tthisag246 <tthisag246@gmail.com>
2024-02-16 21:45:07 +0900

    feat: 매개변수와 반환값을 List 형태로 수정
    #16

--- src/main/java/com/gdsc/goodeat/domain/FoodScraper.java
+++ src/main/java/com/gdsc/goodeat/domain/FoodScraper.java
@@ -5,17 +5,19 @@ import static com.gdsc.goodeat.exception.FoodExceptionType.FOOD_SCRAPING_FAILED;
 
 import com.gdsc.goodeat.exception.FoodException;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class FoodScraper implements AutoCloseable {
-  private static final String BASE_URL = "https://www.tasteatlas.com/";
+public class FoodScraper {
+  private static final String TASTEATLAS_URL = "https://www.tasteatlas.com/";
+  private static final String FOOD_IMG_NOT_FOUND_URL = "https://storage.googleapis.com/goodeat/food_image_not_found.jpg";
   private static final Duration DEFAULT_IMPLICIT_WAIT_DURATION = Duration.ofSeconds(5);
   private static final String SEARCH_INPUT_SELECTOR = "input[placeholder='Search locations or food']";
   private static final String FIRST_RESULT_SELECTOR = "img.food-image.ng-scope:not(.emblem-image)";
@@ -24,43 +26,47 @@ public class FoodScraper implements AutoCloseable {
 
   private final WebDriver driver;
 
-  private final Logger log = LoggerFactory.getLogger(CurrencyConverter.class);
 
   public FoodScraper() {
     this.driver = new ChromeDriver();
   }
 
-  public FoodInfo scrape(String foodName) {
-    try {
-      driver.get(BASE_URL);
+  public List<FoodInfo> scrape(List<String> foodList) {
+    List<FoodInfo> foodInfoList = new ArrayList<>();
 
-      // input food name & search
-      WebElement searchInput = driver.findElement(By.cssSelector(SEARCH_INPUT_SELECTOR));
-      searchInput.click();
-      searchInput.sendKeys(foodName);
-      driver.manage().timeouts().implicitlyWait(DEFAULT_IMPLICIT_WAIT_DURATION);
+    for (String foodName : foodList) {
+      try {
+        driver.get(TASTEATLAS_URL);
 
-      // get search result & enter page
-      WebElement searchResult = getSearchResult();
-      searchResult.click();
+        // input food name & search
+        WebElement searchInput = driver.findElement(By.cssSelector(SEARCH_INPUT_SELECTOR));
+        searchInput.click();
+        searchInput.sendKeys(foodName);
+        driver.manage().timeouts().implicitlyWait(DEFAULT_IMPLICIT_WAIT_DURATION);
 
-      driver.manage().timeouts().implicitlyWait(DEFAULT_IMPLICIT_WAIT_DURATION);
-      // extract info
-      return extractFoodInfo();
-    } catch (FoodException e) {
-      log.info(e.exceptionType().getExceptionMessage()); // FOOD NOT FOUND
-      return new FoodInfo();
-    } catch (NoSuchElementException e) {
-      throw new FoodException(FOOD_SCRAPING_FAILED);
+        // get search result & enter page
+        WebElement searchResult = getSearchResult();
+        searchResult.click();
+        driver.manage().timeouts().implicitlyWait(DEFAULT_IMPLICIT_WAIT_DURATION);
+
+        // extract info
+        foodInfoList.add(extractFoodInfo());
+      } catch (FoodException e) {
+        foodInfoList.add(new FoodInfo(FOOD_IMG_NOT_FOUND_URL, ""));
+      } catch (NoSuchElementException e) {
+        driver.quit();
+        throw new FoodException(FOOD_SCRAPING_FAILED);
+      }
     }
+
+    driver.quit();
+
+    return foodInfoList;
   }
 
   private WebElement getSearchResult() {
-    WebElement firstResult;
-
     try {
-      firstResult = driver.findElement(By.cssSelector(FIRST_RESULT_SELECTOR));
-      return firstResult;
+      return driver.findElement(By.cssSelector(FIRST_RESULT_SELECTOR));
     } catch (NoSuchElementException e) {
       throw new FoodException(FOOD_NOT_FOUND); // Food Not Found
     }
@@ -72,27 +78,27 @@ public class FoodScraper implements AutoCloseable {
     try {
       foodInfo.setImage(driver.findElement(By.cssSelector(IMAGE_SELECTOR)).getAttribute("src"));
     } catch (NoSuchElementException e) {
-      log.info("IMG NOT FOUND");
+      foodInfo.setImage(FOOD_IMG_NOT_FOUND_URL);
     }
 
     try {
       foodInfo.setDescription(driver.findElement(By.cssSelector(DESCRIPTION_SELECTOR)).getText()
           .replaceAll("\\n{2,}", "\n"));
     } catch (NoSuchElementException e) {
-      log.info("DESCRIPTION NOT FOUND");
+      foodInfo.setDescription("");
     }
 
     return foodInfo;
   }
 
-  @Override
-  public void close() throws Exception {
-    driver.quit();
-  }
-
   @Data
+  @AllArgsConstructor
   public static class FoodInfo {
     private String image;
     private String description;
+
+    public FoodInfo() {
+
+    }
   }
 }
