37eaf4fe88eda4b981bd15b09baa0eb352bb3820
hong-sile <hyukjoon.dev@gmail.com>
2024-02-20 17:56:42 +0900

    feat: FakeOcrReader 를 GeminiOcrReader로 변경
    #31

--- src/main/java/com/gdsc/goodeat/service/ReconfigurationService.java
+++ src/main/java/com/gdsc/goodeat/service/ReconfigurationService.java
@@ -7,8 +7,7 @@ import com.gdsc.goodeat.domain.FoodScraper;
 import com.gdsc.goodeat.domain.FoodScraper.FoodInfo;
 import com.gdsc.goodeat.domain.Language;
 import com.gdsc.goodeat.domain.MenuItem;
-import com.gdsc.goodeat.domain.OcrReaderComposite;
-import com.gdsc.goodeat.domain.Price;
+import com.gdsc.goodeat.domain.OcrReader;
 import com.gdsc.goodeat.domain.TranslationClient;
 import com.gdsc.goodeat.dto.ReconfigureRequest;
 import com.gdsc.goodeat.dto.ReconfigureResponse;
@@ -21,10 +20,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ReconfigurationService {
 
-  private final OcrReaderComposite readerComposite;
   private final TranslationClient translationClient;
   private final CurrencyConverter currencyConverter;
   private final FoodScraper foodScraper;
+  private final OcrReader ocrReader;
 
   /**
    * 1. 이미지 읽고, 2. 읽은 값 통해서 크롤링을 먼저하고 3. 이후, 크로링한값 + 메뉴 이름을 번역 4. 환율 계산
@@ -37,9 +36,7 @@ public class ReconfigurationService {
     final Currency userCurrency = Currency.fromCurrencyName(request.userCurrencyName());
 
     //menuName
-    final List<MenuItem> menuItems = readerComposite.readMenu(
-        originLanguage, request.base64EncodedImage()
-    );
+    final List<MenuItem> menuItems = ocrReader.read(request.base64EncodedImage());
     //음식 정보 크롤링 : 조회 후 설명 번역
     final List<FoodInfo> foodInfos = createFoodInfos(menuItems, userLanguage);
     //환율 계산
@@ -79,7 +76,6 @@ public class ReconfigurationService {
   ) {
     final List<Double> prices = menuItems.stream()
         .map(MenuItem::price)
-        .map(Price::amount)
         .toList();
     return currencyConverter.convert(
         prices, originCurrency.getISO4217Code(), userCurrency.getISO4217Code()
--- src/test/java/com/gdsc/goodeat/fake/FakeOcrReaderFrench.java
+++ src/test/java/com/gdsc/goodeat/fake/FakeOcrReaderFrench.java
@@ -1,10 +1,7 @@
 package com.gdsc.goodeat.fake;
 
-import com.gdsc.goodeat.domain.Currency;
-import com.gdsc.goodeat.domain.Language;
 import com.gdsc.goodeat.domain.MenuItem;
 import com.gdsc.goodeat.domain.OcrReader;
-import com.gdsc.goodeat.domain.Price;
 import java.util.List;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -16,13 +13,8 @@ public class FakeOcrReaderFrench implements OcrReader {
   @Override
   public List<MenuItem> read(final String base64encodedImage) {
     return List.of(
-        new MenuItem("Beef bourguignon", new Price(Currency.EURO, 2000.0)),
-        new MenuItem("Foie gras", new Price(Currency.EURO, 1000.0))
+        new MenuItem("Beef bourguignon", 2000.0),
+        new MenuItem("Foie gras", 1000.0)
     );
-  }
-
-  @Override
-  public Language supportedLanguage() {
-    return Language.FRENCH;
   }
 }
